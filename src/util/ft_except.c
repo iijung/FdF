05cd1a612c8721f670d76dfb2f973d1e76efd740
iijung <987kmj4@naver.com>
2022-12-22 06:36:25 +0900

    fix: save errno
    AS-IS
    - errno is changed when ft_putstr_fd throws an error
    
    TO-BE
    - save and restore errno

--- src/util/ft_except.c
+++ src/util/ft_except.c
@@ -6,7 +6,7 @@
 /*   By: minjungk <minjungk@student.42seoul.kr>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/12/04 03:11:34 by minjungk          #+#    #+#             */
-/*   Updated: 2022/12/09 06:05:22 by minjungk         ###   ########.fr       */
+/*   Updated: 2022/12/22 06:35:50 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,8 @@
 
 void	ft_except(int condition, char *file, int line, int is_exit)
 {
+	const int	save_errno = errno;
+
 	if (condition == 0)
 	{
 		errno = 0;
@@ -22,6 +24,7 @@ void	ft_except(int condition, char *file, int line, int is_exit)
 	ft_putstr_fd(file, 2);
 	write(2, ":", 1);
 	ft_putnbr_fd(line, 2);
+	errno = save_errno;
 	perror(":");
 	if (is_exit)
 		exit(EXIT_FAILURE);
