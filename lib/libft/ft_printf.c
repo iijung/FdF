5488a87b46c2de786321102c720bc0fadf003a54
iijung <987kmj4@naver.com>
2024-08-18 06:53:39 +0900

    chore: update libft + ft_printf + get_next_line

--- lib/libft/ft_printf.c
+++ lib/libft/ft_printf.c
@@ -6,50 +6,64 @@
 /*   By: minjungk <minjungk@student.42seoul.>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/07/17 04:45:14 by minjungk          #+#    #+#             */
-/*   Updated: 2022/11/23 14:41:52 by minjungk         ###   ########.fr       */
+/*   Updated: 2024/06/13 04:52:38 by minjungk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-static int	parse_tokens(t_list *lst, va_list ap)
-{
-	int		len;
-	int		total;
-	t_token	*token;
-
-	total = 0;
-	while (lst && lst->content)
-	{
-		token = lst->content;
-		if (ft_parse_token(token, ap) < 0 || token->out == 0)
-			return (-1);
-		if (token->width == 0)
-			token->width = ft_strlen(token->out);
-		len = write(1, token->out, token->width);
-		if (len < 0)
-			return (-1);
-		total += len;
-		lst = lst->next;
-	}
-	return (total);
-}
+#include "ft_printf.h"
+#include "ft_printf_util.h"
 
 int	ft_printf(const char *format, ...)
 {
-	t_list	*head;
 	int		total;
 	va_list	ap;
 
-	head = 0;
-	total = 0;
-	if (format == 0 || format[0] == 0)
-		return (0);
 	va_start(ap, format);
-	total = ft_make_tokens(&head, format);
-	if (total == 0)
-		total = parse_tokens(head, ap);
-	ft_lstclear(&head, ft_free_token);
+	total = ft_vprintf(format, ap);
 	va_end(ap);
+	return (total);
+}
+
+int	ft_dprintf(int fd, const char *format, ...)
+{
+	int		total;
+	va_list	ap;
+
+	va_start(ap, format);
+	total = ft_vdprintf(fd, format, ap);
+	va_end(ap);
+	return (total);
+}
+
+int	ft_vprintf(const char *format, va_list ap)
+{
+	return (ft_vdprintf(STDOUT_FILENO, format, ap));
+}
+
+int	ft_vdprintf(int fd, const char *format, va_list ap)
+{
+	t_token	*token;
+	size_t	len;
+	int		ret;
+	int		total;
+
+	len = 0;
+	total = 0;
+	while (format && format[len] != '\0')
+	{
+		ret = -1;
+		token = ft_make_token(format + len);
+		if (0 == ft_parse_token(token, ap) && token->out)
+		{
+			if (token->width == 0)
+				token->width = ft_strlen(token->out);
+			ret = write(fd, token->out, token->width);
+			total += ret;
+			len += ft_strlen(token->in);
+		}
+		ft_free_token(token);
+		if (ret < 0)
+			return (0);
+	}
 	return (total);
 }
